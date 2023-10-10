242a0fd6bd0aefeda29b8c03e64d3dd7a6312023
nichuanfang <f18326186224@gmail.com>
2023-10-10 11:49:58 +0800

    ✨ feat: 音乐开发工具包

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# music-tool-kit
-music-tool-kit 音乐工具箱
+# music-tool-kit 音乐工具箱
+
+## 安装
+
+```bash
+pip install -U music-tool-kit
+```
+
+## 使用
+
+- 音乐下载
+
+```bash
+mtk download 网址 输出 [封面url]
+```
+
+> tips: 输出格式为 歌曲名[-歌手] 输出歌曲格式为 mp3, 封面 url 可选
+
+- 音乐剪辑
+
+```bash
+mtk clip 输入的文件 开始时间 结束时间
+```
+
+> tips: 时间格式为: 00:00:00
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('mtk/requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read()
+
+setup(
+    name='music-tool-kit',
+    version='0.0.3',
+    description='A tool kit for music download and clip',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    url='https://github.com/nichuanfang/music-tool-kit',
+    author='Nichuan Fang',
+    author_email='f18326186224@gmail.com',
+    license='MIT',
+    platforms='any',
+    keywords=[
+        'mtk'
+        'music',
+        'musictool',
+        'musicdownload',
+        'musicclip',
+        'musickit',
+        'musictoolkit',
+    ],
+    packages=find_packages(exclude=['tests']),
+    package_data={
+        'mtk': ['*.txt'],
+    },
+    require_python='>=3.8',
+    install_requires=[
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mtk = mtk.__main__:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: Chinese (Simplified)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False
+)
