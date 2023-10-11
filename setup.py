885a743a44481c94cf7cb56421f58616cb46fa9f
nichuanfang <f18326186224@gmail.com>
2023-10-11 19:29:14 +0800

    ✨ feat: 测试自动发版

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='music-tool-kit', 
     version='0.2.5',
     description='A tool kit for music download and clip',
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/markdown', 
     long_description=readme,
     url='https://github.com/nichuanfang/music-tool-kit', 
     author='Nichuan Fang',
