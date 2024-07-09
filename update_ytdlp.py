8983f442385016199b97eb717b3bf1ab34a82718
nichuanfang <f18326186224@gmail.com>
2024-07-09 12:54:31 +0800

    feat: 测试自动更新yt-dlp版本

--- update_ytdlp.py
+++ update_ytdlp.py
@@ -1,10 +1,9 @@
 import requests
 import re
 
-# 获取当前 requirements.txt 中的 yt-dlp 版本
-
 
 def get_current_ytdlp_version():
+    # 获取当前 requirements.txt 中的 yt-dlp 版本
     with open('requirements.txt', 'r') as file:
         for line in file:
             if line.startswith('yt-dlp'):
@@ -22,10 +21,9 @@ def get_latest_ytdlp_version():
         return latest_version
     return None
 
-# 更新 requirements.txt 文件中的 yt-dlp 版本
-
 
 def update_requirements_file(new_version):
+    # 更新 requirements.txt 文件中的 yt-dlp 版本
     with open('requirements.txt', 'r') as file:
         lines = file.readlines()
     with open('requirements.txt', 'w') as file:
@@ -35,10 +33,9 @@ def update_requirements_file(new_version):
             else:
                 file.write(line)
 
-# 主逻辑
-
 
 def main():
+    # 主逻辑
     current_version = get_current_ytdlp_version()
     latest_version = get_latest_ytdlp_version()
 
@@ -46,13 +43,16 @@ def main():
         update_requirements_file(latest_version)
         print(
             f'Updated yt-dlp from {current_version} to {latest_version} in requirements.txt')
-
+        author_email = 'git config --global user.email "f18326186224@gmail.com"'
+        author_username = 'git config --global user.name "AutoUpdater"'
         # 使用 Git 提交更改
         commit_message = f'Update yt-dlp to version {latest_version}'
         commit_command = f'git commit -am "{commit_message}"'
         push_command = 'git push origin main'
 
         import subprocess
+        subprocess.run(author_email, shell=True)
+        subprocess.run(author_username, shell=True)
         subprocess.run(commit_command, shell=True)
         subprocess.run(push_command, shell=True)
     else:
